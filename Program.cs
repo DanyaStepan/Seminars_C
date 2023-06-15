cdd2c337eea25c2be97b43979560de67d08bc937
Daria <dora.mils@yandex.ru>
2023-06-16 03:40:34 +0700

    Домашнее задание

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 2: 
+// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// System.Console.Write("Введите первое число -> ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите второе число -> ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+
+// if (a > b)
+// {
+//     System.Console.Write($"Большее число: {a}, меньшее число: {b}");
+// }
+// else
+// {
+//     System.Console.Write($"Большее число: {b}, меньшее число: {a}");
+// }
+
+
+// Задача 4: 
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// System.Console.Write("Введите первое число -> ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите второе число -> ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите третье число -> ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+
+
+// if ((num1 > num2) & (num1 > num3))
+// {
+//     System.Console.Write($"Максимальным числом является {num1} ");
+// }
+// else if (num2 > num3)
+// {
+//     System.Console.Write($"Максимальным числом является {num2} ");
+// }
+// else 
+// {
+//    System.Console.Write($"Максимальным числом является {num3} "); 
+// }
+
+// Задача 6: 
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
