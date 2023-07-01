262efeea5706e3c154fae394bc10770a62d68810
Daria <dora.mils@yandex.ru>
2023-07-02 02:06:12 +0700

    Семинар 5

--- Program.cs
+++ Program.cs
@@ -384,3 +384,78 @@
 // System.Console.Write("Input maximal elements of array: ");
 // int maxValue = Convert.ToInt32(Console.ReadLine());
 // PrintArray(CreateRandomArray(size, minValue, maxValue));
+
+
+//СЕМИНАР 5.
+
+// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+// System.Console.Write("Введите колличество элементов массива -> ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int [] randomArray = new int[size];
+
+// void arr(int size)
+// {
+//     for (int i = 0; i < size; i++)
+//     {
+//         randomArray[i]=new Random().Next(99,999);
+//         System.Console.Write(randomArray[i] + " ");
+//     }
+// }
+
+
+// int ColVo(int[] randomArray)
+// {
+//     int ColVo = 0;
+//     for (int i = 0; i < randomArray.Length; i++)
+//     {
+//         if(randomArray[i] % 2==0)
+//         ColVo = ColVo + 1;
+//     }
+//     System.Console.WriteLine();
+//     return ColVo;
+// }
+
+// arr(size);
+// System.Console.Write($"Количество чётных чисел в массиве {ColVo(randomArray)}");
+
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+// Console.Write("Введите размер массива -> ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int[] numbers = new int[size];
+// FillArrayRandomNumbers(numbers);
+// Console.Write("Вот наш массив: ");
+// PrintArray(numbers);
+// int sum = 0;
+
+// for (int a = 0; a < numbers.Length; a+=2)
+//     sum = sum + numbers[a];
+
+//     Console.Write($"Всего {numbers.Length} чисел(а), сумма элементов на нечётных позициях = {sum}");
+
+// void FillArrayRandomNumbers(int[] numbers)
+// {
+//     for(int i = 0; i < numbers.Length; i++)
+//         {
+//             numbers[i] = new Random().Next(1,100);
+//         }
+// }
+// void PrintArray(int[] numbers)
+// {
+//     for(int i = 0; i < numbers.Length; i++)
+//         {
+//             Console.Write(numbers[i] + " ");
+//         }
+//     Console.WriteLine();
+// }
+
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+ 
