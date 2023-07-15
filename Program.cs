70f6255a8c12c7c5fdc9a2b42245b877db537926
Daria <dora.mils@yandex.ru>
2023-07-15 17:27:28 +0700

    Homeworks

--- Program.cs
+++ Program.cs
@@ -979,3 +979,96 @@ m = 3, n = 4.
 // }
 
 
+// СЕМИНАР 9. ИТОГ.
+
+// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+// void PrintNum(int num)
+// {
+//     if(num>=1)
+//     {
+//         System.Console.Write(num + "  ");
+//         PrintNum(num-1);
+
+//     }
+// }
+// System.Console.Write("Input num -> ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// PrintNum(num);
+// System.Console.WriteLine();
+
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+// int FindSum(int m, int n) 
+// { 
+//     if (m == 0)  
+//     { 
+//         return (n * (n + 1)) / 2; 
+//     }             
+//     else if (n == 0)  
+//     { 
+//         return (m * (m + 1)) / 2; 
+//     }        
+//     else if (m == n)  
+//     { 
+//         return m; 
+//     }                        
+//     else if (m < n)  
+//     { 
+//         return n + FindSum(m, n - 1); 
+//     }  
+//     else 
+//     { 
+//         return n + FindSum(m, n + 1); 
+//     } 
+// } 
+// 
+// Console.Write("Input first num -> "); 
+// int m = Convert.ToInt32(Console.ReadLine()); 
+// Console.Write("Input second num -> "); 
+// int n = Convert.ToInt32(Console.ReadLine()); 
+// Console.WriteLine($"The sum of natural elements is equal to -> {FindSum(m, n)}");
+
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+// int Akkerman(int m, int n)
+// {
+//     if (m == 0)
+//     {
+//         return n + 1;
+//     }
+//     else if (n == 0 && m > 0)
+//     {
+//         return Akkerman(m - 1, 1);
+//     }
+//     else
+//     {
+//         return (Akkerman(m - 1, Akkerman(m, n - 1)));
+//     }
+// }
+
+// void AkkermanFunction(int m, int n)
+// {
+
+//     System.Console.Write($"Функция Аккермана ({m},{n}) -> {Akkerman(m, n)}");
+//     System.Console.WriteLine();
+// }
+
+// System.Console.Write("Input m -> ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Input n -> ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// AkkermanFunction(m, n);
